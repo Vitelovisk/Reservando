9ecbf50ac8ec75da07aa1372565718f49ff7da6b
Vitelovisk <62072833+Vitelovisk@users.noreply.github.com>
2023-11-27 17:17:25 -0300

    Update Conexao.php

--- panel/config/Conexao.php
+++ panel/config/Conexao.php
@@ -1,8 +1,8 @@
 <?php
   $host = "aws-reservando-db-mysql.c9joiyhrzm9x.sa-east-1.rds.amazonaws.com";
   $user = "admin";
-  $pass = "";
-  $db = "yqN5ZbUGwb5K564N2koi";
+  $pass = "yqN5ZbUGwb5K564N2koi";
+  $db = "";
 
 $Conexao =  new mysqli($host,$user,$pass,$db);
 

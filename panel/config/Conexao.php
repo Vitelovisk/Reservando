d9b21a68f583aec03d9765cd175669706bf21965
Vitelovisk <62072833+Vitelovisk@users.noreply.github.com>
2023-11-29 18:46:54 -0300

    correcao de nao aparecer na tela

--- panel/config/Conexao.php
+++ panel/config/Conexao.php
@@ -1,8 +1,8 @@
-<?php
+GNU nano 6.2                                           Conexao.php                                                    <?php
   $host = "aws-reservando-db-mysql.c9joiyhrzm9x.sa-east-1.rds.amazonaws.com";
   $user = "admin";
   $pass = "yqN5ZbUGwb5K564N2koi";
-  $db = "";
+  $db = "reservando";
 
 $Conexao =  new mysqli($host,$user,$pass,$db);
 
--- panel/config/db.php
+++ panel/config/db.php
@@ -1,4 +1,11 @@
 <?php
-    $mysqli = new mysqli("aws-reservando-db-mysql.c9joiyhrzm9x.sa-east-1.rds.amazonaws.com", "admin", "yqN5ZbUGwb5K564N2koi", "aws-reservando-db-mysql");
-
+    $mysqli = new mysqli("aws-reservando-db-mysql.c9joiyhrzm9x.sa-east-1.rds.amazonaws.com", "admin", "yqN5ZbUGwb5K564N>
 ?>
+
+
+
+
+
+
+
+
